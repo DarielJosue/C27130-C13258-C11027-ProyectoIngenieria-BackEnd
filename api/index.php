187bb638b51214f14c8f27ef3919e7a68c80d3dc
DARIEL.BENAVIDES <DARIEL.BENAVIDES@ucr.ac.cr>
2025-06-07 21:32:10 -0600

    configuraciones para vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,16 @@
 <?php
 
+$source = __DIR__ . '/../database/database.sqlite';
+$target = '/tmp/database.sqlite';
 
+if (file_exists($source) && !file_exists($target)) {
+    copy($source, $target);
+}
+if (!file_exists('/tmp/cache')) {
+    mkdir('/tmp/cache', 0777, true);
+}
+if (!file_exists('/tmp/sessions')) {
+    mkdir('/tmp/sessions', 0777, true);
+}
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
